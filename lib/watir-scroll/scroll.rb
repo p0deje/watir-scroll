5007405aa482a8d6358f388791426b5ab8c1cadb
Alex Rodionov <p0deje@gmail.com>
2013-08-01 13:21:05 +0700

    handles aliases, uknown scrolling points

--- lib/watir-scroll/scroll.rb
+++ lib/watir-scroll/scroll.rb
@@ -12,16 +12,18 @@ module Watir
       #
       def to(param)
         args = case param
-               when :top
+               when :top, :start
                  'window.scrollTo(0, 0);'
                when :center
                  'window.scrollTo(document.body.scrollWidth / 2, document.body.scrollHeight / 2);'
-               when :bottom
-                 'window.scrollTo(document.body.scrollWidth, document.body.scrollHeight);'
+               when :bottom, :end
+                 'window.scrollTo(0, document.body.scrollHeight);'
                when Watir::Element
                  ['arguments[0].scrollIntoView();', param]
                when Array
                  ['window.scrollTo(arguments[0], arguments[1]);', Integer(param[0]), Integer(param[1])]
+               else
+                 raise ArgumentError, "Don't know how to scroll to: #{param}!"
                end
 
          @browser.execute_script(*args)
